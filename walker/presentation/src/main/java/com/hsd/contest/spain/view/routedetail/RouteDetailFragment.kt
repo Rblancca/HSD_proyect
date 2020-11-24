c52d37f445ee5bb5d48d11ecf85f9ab5a533970e
Rocio <rocioblan@hotmail.com>
2020-11-24 19:44:24 +0100

    huawei mobile service config

--- walker/presentation/src/main/java/com/hsd/contest/spain/view/routedetail/RouteDetailFragment.kt
+++ walker/presentation/src/main/java/com/hsd/contest/spain/view/routedetail/RouteDetailFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.hsd.contest.domain.URL_IMAGE
 import com.hsd.contest.spain.R
